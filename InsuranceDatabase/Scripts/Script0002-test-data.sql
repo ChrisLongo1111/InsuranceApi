7251a77a782a887dce39ff7836c267636ac30a28
Chris Longo <ChrisLongo@outlook.com>
2022-03-01 08:09:10 -0500

    Added Test Data

---
+++ InsuranceDatabase/Scripts/Script0002-test-data.sql
@@ -0,0 +1,21 @@
+﻿delete Dependent
+delete Employee
+delete Quote
+DBCC CHECKIDENT ('Dependent', RESEED, 0);
+DBCC CHECKIDENT ('Employee', RESEED, 0);
+DBCC CHECKIDENT ('Quote', RESEED, 0);
+
+DECLARE @LASTID bigint;
+insert Quote (Name) values ('First')
+SET @LASTID = @@IDENTITY
+insert Employee (FirstName, LastName, QuoteId) values ('Chris', 'Longo', @LASTID)
+SET @LASTID = @@IDENTITY
+insert Dependent (FirstName, LastName, EmployeeId) values ('Denise', 'Longo', @LASTID)
+insert Dependent (FirstName, LastName, EmployeeId) values ('Matt', 'Longo', @LASTID)
+
+insert Quote (Name) values ('Second')
+SET @LASTID = @@IDENTITY
+insert Employee (FirstName, LastName, QuoteId) values ('Chris', 'Long', @LASTID)
+SET @LASTID = @@IDENTITY
+insert Dependent (FirstName, LastName, EmployeeId) values ('Denise', 'Long', @LASTID)
+insert Dependent (FirstName, LastName, EmployeeId) values ('Matt', 'Long', @LASTID)
